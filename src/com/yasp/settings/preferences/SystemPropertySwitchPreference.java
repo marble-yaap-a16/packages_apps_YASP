4d13d6fd1293be3ee0579cc62ea2c03d8987bf1f
cjybyjk <cjybyjk@zjnu.edu.cn>
2023-04-17 20:18:13 +0530

    Yasp: Add SystemPropertySwitchPreference

---
+++ src/com/yasp/settings/preferences/SystemPropertySwitchPreference.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2017 AICP
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yasp.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+import androidx.preference.TwoStatePreference;
+
+public class SystemPropertySwitchPreference extends SwitchPreference {
+
+    public SystemPropertySwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemPropertiesStore());
+    }
+
+    public SystemPropertySwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemPropertiesStore());
+    }
+
+    public SystemPropertySwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setPreferenceDataStore(new SystemPropertiesStore());
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        boolean value;
+
+        if (defaultValue instanceof Boolean) {
+            value = (Boolean) defaultValue;
+        } else {
+            value = false;
+        }
+
+        if (restorePersistedValue) {
+            value = getPersistedBoolean(value);
+        }
+
+        setChecked(value);
+    }
+}
